45aab8177f49d82dee1f884c1ce516a961c43e5a
KARISHMA <KARISHMA@LAPTOP-SD1F208B>
2022-07-27 17:09:42 +0530

    fertilizer soil testing

---
+++ src/shetakriputra/fertilizer.java
@@ -0,0 +1,10 @@
+package shetakriputra;
+
+public class fertilizer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/shetakriputra/soiltesting.java
@@ -0,0 +1,10 @@
+package shetakriputra;
+
+public class soiltesting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
